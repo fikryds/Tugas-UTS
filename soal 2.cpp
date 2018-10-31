c43d746660e26616626194a904195f9e314ba1b1
fikryds <fikryds03@gmail.com>
2018-10-31 13:25:11 +0700

    frist commit

---
+++ soal 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{int N,X,T,Batas;
+N = 69  ;
+Batas = 69 + 100;
+X = 20;
+T = 69;
+while( T <= Batas)
+     { T = T + X;
+       X = X + 10;
+     }
+ cout << T;
+
+}
+
